5e55f0b97abd91575b6c2f7421c5151f3472fa23
Roy Voorbach <royvb@hotmail.com>
2024-01-28 17:22:40 +0100

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Nums = ( {persons} ) => {
-  console.log(persons)
-
   return (
     <div>
       {persons.map(persons => <p key={persons.name}>
@@ -30,17 +29,23 @@ const PersonForm = ( {addName, newName, handleNameChange, newNumber, handleNumbe
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
 
   const [f, setF] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+        setFilteredPersons(response.data)
+      })
+  }, [])
 
   const addName = (event) => {
     event.preventDefault()
@@ -48,7 +53,6 @@ const App = () => {
       name: newName,
       number: newNumber
     }
-    console.log(personObject)
 
     const included = persons.some(person => person.name === personObject.name)
       
@@ -79,6 +83,9 @@ const App = () => {
 
   }
 
+  console.log(persons)
+  console.log(filteredPersons)
+
   return (
     <div>
       <h1>Phonebook</h1>
@@ -94,3 +101,12 @@ const App = () => {
 }
 
 export default App
+
+/*
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+*/
