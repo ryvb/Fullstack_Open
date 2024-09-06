149c6d08122d01bb7a522e9cb2c95e05bd72c98d
Roy Voorbach <royvb@hotmail.com>
2024-09-06 19:28:53 +0200

    exercises 9.8-9.9

---
+++ part9/patientor/patientor_backend/index.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from "cors";
+const app = express();
+
+const allowedOrigins = ['http://localhost:5173'];
+const options: cors.CorsOptions = {
+    origin: allowedOrigins
+};
+
+app.use(cors(options));
+app.use(express.json());
+
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
